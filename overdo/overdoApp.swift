07dfe02219ca59f7aabf03a14e47abf64a1c42c8
Tomas Svoboda <hatchery41@icloud.com>
2026-05-16 17:26:45 +0200

    Initial Commit

---
+++ overdo/overdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  overdoApp.swift
+//  overdo
+//
+//  Created by tomas on 16.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct overdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
